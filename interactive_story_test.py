9ae55b85074a242ffacea971f18cf94f6322480d
NathanielMH <nathanielmitrani@gmail.com>
2022-06-24 18:26:10 +0200

    Update interactive_story_test.py

--- interactive_story_test.py
+++ interactive_story_test.py
@@ -8,13 +8,24 @@ S.augment_story("The sheep, who is not one to be scared easily, calmly ignored t
                 "R: abandoned the idea and started crying", "The sheep greeted the "
                                                             "wolf, accepted the "
                                                             "compliment, "
-                                                            "and they started to talk")
+                                                            "and they started to talk. The wolf then L: Took "
+                                                            "advantage of that to scare her even more, R: Took the "
+                                                            "opportunity and became "
+                                                            "friends with her")
 
 S.left.augment_story(
     "The wolf was too lonely to let a potential friend go. He begged the sheep to listen, and she did.",
     "The sheep saw despair in the wolf's eyes and decided to walk up to him.")
 
+S.right.augment_story("The sheep told the wolf that he would not get any friends by being scary, and wandered off.",
+                      "After a while, the sheep decided to invite the wolf to her house to play. It was the wolf's "
+                      "first invitation and he was extremely happy.")
+
 print(S.story, S.left.story, S.left.left.story, sep="\n", end=" THE END")
 print()
 print(S.story, S.left.story, S.left.right.story, sep="\n", end=" THE END")
+print()
+print(S.story, S.right.story, S.right.left.story, sep="\n", end=" THE END")
+print()
+print(S.story, S.right.story, S.right.right.story, sep="\n", end=" THE END")
 print()
